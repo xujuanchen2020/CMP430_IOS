3efef93ac625eadab712bb3538389e7c09548222
xujuan_chen <60618009+xujuanchen2020@users.noreply.github.com>
2020-09-11 21:11:55 -0400

    Create UILabel.swift

---
+++ touchCard/UILabel.swift
@@ -0,0 +1,36 @@
+import UIKit
+
+class ViewController: UIViewController {
+    
+//    var flipCount: Int = 0
+    var flipCount = 0 {
+        didSet{
+            flipCountLabel.text = "Flips: \(flipCount)"
+        }
+    }
+    
+    @IBAction func touchCard(_ sender: UIButton) {
+        flipCard(withEmoji: "👻", on: sender)
+        flipCount += 1
+        
+    }
+    
+    @IBAction func secondTouchCard(_ sender: UIButton) {
+        flipCard(withEmoji: "🎃", on: sender)
+        flipCount += 1
+        
+    }
+    
+    @IBOutlet weak var flipCountLabel: UILabel!
+    
+    func flipCard(withEmoji emoji: String, on button:UIButton){
+        if button.currentTitle == emoji {
+            button.setTitle("", for: UIControl.State.normal)
+            button.backgroundColor = #colorLiteral(red: 0.9372549057, green: 0.3490196168, blue: 0.1921568662, alpha: 1)
+        }
+        else{
+            button.setTitle(emoji, for: UIControl.State.normal)
+            button.backgroundColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        }
+    }   
+}
