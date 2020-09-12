050e4277f083135502b1ca660f2938f65e63858e
xujuan_chen <60618009+xujuanchen2020@users.noreply.github.com>
2020-09-12 09:13:42 -0400

    Create ArrayButtons.swift

---
+++ touchCard/ArrayButtons.swift
@@ -0,0 +1,38 @@
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var emojiChoices = ["👻","🎃","🍭","🍡"]
+    
+    var flipCount = 0 {
+        didSet{
+            flipCountLabel.text = "Flips: \(flipCount)"
+        }
+    }
+    
+    @IBOutlet var cardButtons: [UIButton]!
+    
+    @IBOutlet weak var flipCountLabel: UILabel!
+    
+    @IBAction func touchCard(_ sender: UIButton) {
+        if let cardNumber = cardButtons.firstIndex(of: sender){
+            print (" cardNumber = \(cardNumber)")
+            flipCard(withEmoji: emojiChoices[cardNumber], on: sender)
+            flipCount += 1
+        }
+        else{
+            print ("it is not in the cardNumber!")
+        }
+    }
+    
+    func flipCard(withEmoji emoji: String, on button:UIButton){
+        if button.currentTitle == emoji {
+            button.setTitle("", for: UIControl.State.normal)
+            button.backgroundColor = #colorLiteral(red: 0.9372549057, green: 0.3490196168, blue: 0.1921568662, alpha: 1)
+        }
+        else{
+            button.setTitle(emoji, for: UIControl.State.normal)
+            button.backgroundColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        }
+    }
+}
