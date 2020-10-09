40c2fd1dd94158bf7c439e1e5bdf92c7ff7ec337
xujuan_chen <60618009+xujuanchen2020@users.noreply.github.com>
2020-10-09 16:47:08 -0400

    Create PlayingCardDeck.swift

---
+++ Standford_lectures/lecture_09232020/PlayingCardDeck.swift
@@ -0,0 +1,34 @@
+
+struct PlayingCardDeck {
+    
+    private(set) var cards = [PlayingCard]()
+    
+    mutating func draw() -> PlayingCard? {
+        if cards.count > 0 {
+            return cards.remove(at: cards.count.arc4random)
+        }else{
+            return nil
+        }
+    }
+    
+    init(){
+        for suit in PlayingCard.Suit.all{
+            for rank in PlayingCard.Rank.all{
+                cards.append(PlayingCard(suit: suit, rank: rank))
+            }
+        }
+    }
+    
+}
+
+extension Int {
+    var arc4random: Int {
+        if (self > 0){
+            return Int(arc4random_uniform(UInt32(self)))
+        }else if (self < 0){
+            return -Int(arc4random_uniform(UInt32(abs(self))))
+        }else{
+            return 0
+        }
+    }
+}
